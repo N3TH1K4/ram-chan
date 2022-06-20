15cb789e4fb43443e2a459ffaa9aad6d7c653108
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-06-20 22:09:18 +0530

    Update data_parser.py

--- anibot/utils/data_parser.py
+++ anibot/utils/data_parser.py
@@ -772,10 +772,6 @@ async def get_additional_info(idm, req, ctgry, auth: bool = False, user: int = N
     noban = "**No Banner Image**"
     pic = f"https://img.anili.st/media/{idm}"
     banner = data['bannerImage']
-    if banner == None:
-	banner = noban
-    else:
-	banner = banner
     if req == "desc":
         synopsis = data.get("description")
         return (pic if ctgry == "ANI" else data["image"]["large"]), synopsis
